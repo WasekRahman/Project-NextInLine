7e2c406eb5d92bf5a6ad2ae4289ad59a9bb70e0f
Wasek Rahman <31708476+WasekRahman@users.noreply.github.com>
2020-10-14 11:34:04 -0400

    Added: Post method
    Post method for the "building" table is added. Users can now post info to the database successfully.

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const routes = require("./routes/index");
 const connection = require("./database/connection");
-app.use(routes);
+middleware = require("./middleware.js");
+app.use(middleware);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
--- rest-api/routes/dashboardpage.js
+++ rest-api/routes/dashboardpage.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const dashboardrouter = express.Router();
+const buildingInfo = require("../model/buildingmodel");
 
 dashboardrouter.get("/", (req, res) => {
   res.send("Dashboard");
 });
-dashboardrouter.get("/walmart", (req, res) => {
-  res.send("For walmart");
+dashboardrouter.post("/walmart", async (req, res) => {
+  const info = new buildingInfo({
+    buildingID: req.body.buildingID,
+    name: req.body.name,
+    capacity: req.body.capacity,
+    occupancy: req.body.occupancy,
+  });
+
+  try {
+    const savedInfo = await info.save();
+    res.json(savedInfo);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = dashboardrouter;
