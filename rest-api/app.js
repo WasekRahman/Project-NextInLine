0350da9071831c93bf91f73eafd06edfe915659c
Wasek Rahman <31708476+WasekRahman@users.noreply.github.com>
2020-10-17 00:15:06 -0400

    Updated: Eventpage
    Eventpage updated from POST to GET

--- rest-api/app.js
+++ rest-api/app.js
@@ -3,5 +3,4 @@ const app = express();
 const connection = require("./database/connection");
 middleware = require("./middleware.js");
 app.use(middleware);
-
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
